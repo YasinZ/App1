04e8eadbd48776411494f5ec98ae6fa449857c8d
YasinZ <yasinzuh@gmail.com>
2017-03-28 17:42:11 -0700

    Added pause button and menu

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour {
+    public Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        foreach (var touch in Input.touches)
+            if (touch.phase == TouchPhase.Began)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(touch.position), Vector2.zero);
+
+                if (hit && hit.collider.gameObject.CompareTag("Pause"))
+                    canvas.gameObject.SetActive(!canvas.gameObject.activeSelf);
+
+            }
+    }
+}
