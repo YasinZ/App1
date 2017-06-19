ab16c704f74bff76e84eda61636ce8dd8b7d3203
YasinZ <yasinzuh@gmail.com>
2017-06-19 16:57:35 -0700

    Added new vertical scene for main game

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -24,4 +24,9 @@ public class SceneLoader : MonoBehaviour {
     {
         SceneManager.LoadScene("Scenes/Tutorial");
     }
+
+    public void StartTD()
+    {
+        SceneManager.LoadScene("Scenes/MainMap");
+    }
 }
